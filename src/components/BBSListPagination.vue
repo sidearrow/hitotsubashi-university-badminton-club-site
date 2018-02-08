73389452131185f6583ccc46f559938bfefd0319
sidearrow <sidearrow1219@gmail.com>
2018-02-08 21:30:26 +0900

    mod

--- src/components/BBSListPagination.vue
+++ src/components/BBSListPagination.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    pagination
+    <nav>
+      <ul class="pagination">
+        <li class="page-item">
+          <router-link class="page-link"></router-link>
+        </li>
+        <li class="page-item" v-for="pageNum in pageNums">
+          <router-link class="page-link">{{ pageNum }}</router-link>
+        </li>
+        <li class="page-item">
+          <router-link class="page-link">>></router-link>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pageNums: [1,2,3,4,5],
+    };
+  },
+};
 </script>
--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    member
+    <ul class="nav nav-tabs">
+      <li class="nav-item"><span class="nav-link active" @click="click">1</span></li>
+      <li class="nav-item"><span class="nav-link" @click="click">2</span></li>
+      <li class="nav-item"><span class="nav-link" @click="click">3</span></li>
+      <li class="nav-item"><span class="nav-link" @click="click">4</span></li>
+    </ul>
   </div>
 </template>
 
@@ -11,5 +16,11 @@ export default {
       test: 'aaa',
     }
   },
+  methods: {
+    click: function(e) {
+      document.querySelector('.nav-link.active').classList.remove('active');
+      e.currentTarget.classList.add('active');
+    },
+  }
 };
 </script>
