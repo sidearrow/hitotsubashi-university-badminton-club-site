e312081226a33b685b1d66fdb547dbb81ff3cfc5
sidearrow <sidearrow1219@gmail.com>
2018-09-10 21:28:07 +0900

    commit

--- front/src/pages/top/Index.vue
+++ front/src/pages/top/Index.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <div class="alert alert-warning">
-      <span>こちらはβ版になります。<br/>最新の情報は以下リンク先でご確認下さい。<br/><a href="http://jfn.josuikai.net/circle/sports/badminton/" target="__blank">http://jfn.josuikai.net/circle/sports/badminton/</a></span>
+      <span>こちらはβ版になります。</span>
+      <br/>
+      <span>最新の情報は以下リンク先でご確認下さい。</span>
+      <br/>
+      <a href="http://jfn.josuikai.net/circle/sports/badminton/" target="__blank">http://jfn.josuikai.net/circle/sports/badminton/</a>
     </div>
     <slf-cmp-top-carousel/>
     <div class="jumbotron mt-3">
       <h1 class="text-nowrap">一橋大学<wbr>バドミントン部</h1>
       <hr class="my-4">
-      <p>2017年7月現在、私達一橋大学バドミントン部は男子24名、女子7名の計31名で活動しています。<br/>
-男女・経験者大学始め関係なく、リーグ戦での4部への昇格を目指して日々練習に励んでいます。</p>
+      <p>
+        <span>2017年7月現在、私達一橋大学バドミントン部は男子24名、女子7名の計31名で活動しています。</span>
+        <br/>
+        <span>男女・経験者大学始め関係なく、リーグ戦での4部への昇格を目指して日々練習に励んでいます。</span>
+      </p>
     </div>
   </div>
 </template>
