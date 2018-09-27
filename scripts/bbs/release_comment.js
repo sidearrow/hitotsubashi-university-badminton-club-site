799d1e01f83681204294141332ed6526a294e114
sidearrow <sidearrow1219@gmail.com>
2018-09-27 21:24:25 +0900

    commit

---
+++ scripts/bbs/release_comment.js
@@ -0,0 +1,24 @@
+const firebase = require('firebase')
+const config = require('../config')
+
+require('firebase/firestore')
+firebase.initializeApp(config.firebase)
+const db = firebase.firestore()
+db.settings({timestampsInSnapshots: true})
+
+db
+  .collection('bbs')
+  .get()
+  .then((qs) => {
+    qs.forEach((v) => {
+      db
+        .collection('bbs')
+        .doc(v.id)
+        .update({
+          password: '1111',
+          comments: [{
+            'keep': 0
+          }]
+        })
+    })
+})
