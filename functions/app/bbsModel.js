5f781b8c89bdf7e918e1b8e2b05925c2a5a1d710
sidearrow <sidearrow1219@gmail.com>
2018-10-05 23:23:52 +0900

    commit

--- functions/app/bbsModel.js
+++ functions/app/bbsModel.js
@@ -12,7 +12,7 @@ const getCollectionName = (version) => {
 const formatDate = (rowDate) => {
   const timezoneOffsetDiff = -540 - new Date().getTimezoneOffset()
   const d = new Date(rowDate._seconds * 1000)
-  d.setTime(d.getTime() + 1000 * 60 * timezoneOffsetDiff)
+  d.setTime(d.getTime() - 1000 * 60 * timezoneOffsetDiff)
   const f = (input) => {
     return ('0' + String(input)).substr(-2)
   }
