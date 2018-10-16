d314864a4bd2a51da1cc775a0de9dce1d4e6356b
sidearrow <sidearrow1219@gmail.com>
2018-10-16 21:14:40 +0900

    commit

--- front/tests/unit/pages/pages-top-index.spec.js
+++ front/tests/unit/pages/pages-top-index.spec.js
@@ -3,7 +3,7 @@ import { shallowMount } from '@vue/test-utils'
 import PagesTopIndex from '@/pages/top/Index.vue'
 
 describe('@/pages/top/Index.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('check title', () => {
     const wrapper = shallowMount(PagesTopIndex)
     console.log(wrapper)
     console.log("323")
