5f54f8a95cc89f5614c890659192e313a57fdbe3
sidearrow <sidearrow1219@gmail.com>
2019-03-24 23:54:10 +0900

    commit

--- funtions/app/controllers/bbsController.js
+++ funtions/app/controllers/bbsController.js
@@ -1,23 +1,22 @@
 const modelBbs = require('../models/bbs')
-const modelBbsDelete = require('../models/bbsDelete')
-const modelBbsDateList = require('../models/bbsDateList')
 const util = require('../util')
 const controller = {}
 
 controller.create = async (req, res) => {
-  const now = new Date(Date.now())
+  const now = new Date()
 
-  const data = {}
-  data.title     = req.body.title
-  data.author    = req.body.author
-  data.content   = req.body.content
-  data.password  = req.body.password
-  data.comments  = []
-  data.createdAt = now
-  data.updatedAt = now
+  const data = {
+    title    : req.body.title,
+    author   : req.body.author,
+    content  : req.body.content,
+    password : req.body.password,
+    comments : [],
+    createdAt: now,
+    updatedAt: now,
+    deletedAt: null,
+  }
 
   const id = await modelBbs.create(data)
-  modelBbsDateList.updateNumIncrement(util.getMonthString(data.createdAt))
 
   res.json({
     isSuccess: true,
@@ -29,20 +28,41 @@ controller.show = async (req, res) => {
   const id = req.params.id
   const queryPassword = req.query.password
 
-  res.json(
-    await modelBbs.get(id, queryPassword)
-  )
+  const doc = await modelBbs.get(id)
+  const docData = doc.data()
+
+  const resData = {
+    id       : doc.id,
+    auth     : queryPassword === docData.password,
+    author   : docData.author,
+    title    : docData.title,
+    content  : docData.content,
+    comments : [],
+    createdAt: util.getDateString(docData.createdAt.toDate()),
+    updatedAt: util.getDateString(docData.updatedAt.toDate()),
+  }
+  docData.comments.forEach((v) => {
+    resData.comments.push({
+      author   : v.author,
+      content  : v.content,
+      createdAt: util.getDateString(v.createdAt.toDate()),
+      deletedAt: v.deletedAt && util.getDateString(v.deletedAt.toDate())
+    })
+  })
+
+  res.json(resData)
 }
 
 controller.update = async (req, res) => {
   const id = req.params.id
 
-  const data = {}
-  data.title     = req.body.title
-  data.author    = req.body.author
-  data.content   = req.body.content
-  data.password  = req.body.password
-  data.updatedAt = new Date(Date.now())
+  const data = {
+    title    : req.body.title,
+    author   : req.body.author,
+    content  : req.body.content,
+    password : req.body.password,
+    updatedAt: new Date(Date.now()),
+  }
 
   await modelBbs.update(id, data)
   res.json({
@@ -51,14 +71,13 @@ controller.update = async (req, res) => {
 }
 
 controller.delete = async (req, res) => {
-  const reqId = req.params.id
+  const id = req.params.id
   const reqPassword = req.query.password
 
-  const target = await modelBbs.get(reqId)
-  if (target.password === reqPassword) {
-    await modelBbs.delete(reqId)
-    modelBbsDelete.create(target)
-    modelBbsDateList.updateNumDecrement(util.getMonthString(target.createdAtRaw.toDate()))
+  const targetDoc = await modelBbs.get(id)
+  targetDocData = targetDoc.data()
+  if (targetDocData.password === reqPassword) {
+    await modelBbs.delete(id)
     res.json({isSuccess: true})
   } else {
     res.json({isSuccess: false})
@@ -66,19 +85,19 @@ controller.delete = async (req, res) => {
 }
 
 controller.commentsCreate = async (req, res) => {
-  const reqId = req.params.id
+  const id = req.params.id
 
-  const data = {}
-  data.author    = req.body.author
-  data.content   = req.body.content
-  data.password  = req.body.password
-  data.createdAt = new Date(Date.now())
-  data.isDelete  = false
+  const data = {
+    author   : req.body.author,
+    content  : req.body.content,
+    password : req.body.password,
+    createdAt: new Date(Date.now()),
+    isDelete : false,
+  }
 
-  const target = await modelBbs.get(reqId)
+  const target = await modelBbs.get(id)
   target.comments.push(data)
-  const updateData = { comments: target.comments}
-  await modelBbs.update(reqId, updateData)
+  await modelBbs.update(id, { comments: target.comments })
 
   res.json({isSuccess: true})
 }
@@ -101,14 +120,28 @@ controller.commentsDelete = async (req, res) => {
 }
 
 controller.index = async (req, res) => {
-  const reqId = req.query.id
+  const id = req.query.id
+
+  let qs
+  if (typeof id === 'undefined') {
+    qs = await modelBbs.getPage()
+  } else {
+    qs = await modelBbs.getPage(id)
+  }
 
   let resData = []
-  if (typeof reqId === 'undefined') {
-    resData = await modelBbs.getPage()
-  } else {
-    resData = await modelBbs.getPage(reqId)
-  }
+  let tmpDocData
+  qs.forEach((qs) => {
+    tmpDocData = qs.data()
+    resData.push({
+      id       : qs.id,
+      author   : tmpDocData.author,
+      title    : tmpDocData.title,
+      content  : tmpDocData.content,
+      createdAt: util.getDateString(tmpDocData.createdAt.toDate()),
+      updatedAt: util.getDateString(tmpDocData.updatedAt.toDate()),
+    })
+  })
 
   res.json(resData)
 }
@@ -126,11 +159,6 @@ controller.dateIndex = async (req, res) => {
   const month = Number(req.params.date.substr(4, 2))
 
   const data = await modelBbs.getDateNarrow(year, month)
-  res.json(data)
-}
-
-controller.dateListIndex = async (req, res) => {
-  const data = await modelBbsDateList.get()
   res.json(data)
 }
 
--- funtions/app/models/bbs.js
+++ funtions/app/models/bbs.js
@@ -1,92 +1,53 @@
 const database = require('../firestore')
-const util = require('../util')
 const collectionName = 'bbs'
 
-const model = {}
-const formatResponse = (id, data, isDetail = false, password = null) => {
-  data.id = id
-  data.createdAtRaw = data.createdAt
-  data.updatedAtRaw = data.updatedAt
-  data.createdAt = util.getDateString(data.createdAt.toDate())
-  data.updatedAt = util.getDateString(data.updatedAt.toDate())
+const _ = {}
 
-  // コメント
-  if (isDetail) {
-    data.comments.forEach((v, i) => {
-      if (v.isDelete) {
-        data.comments[i] = { isDelete: true }
-      } else {
-        data.comments[i].createdAt = util.getDateString(v.createdAt.toDate())
-        delete data.comments[i].password
-      }
-    })
-  } else {
-    data.commentsNum = data.comments.length
-    delete data.comments
-  }
-
-  // 認証
-  data.auth = false
-  if (password !== null && data.password === password) {
-    data.auth = true
-  }
-  delete data.password
-
-  return data
+/**
+ * @param  {string} id
+ * @return {FirebaseFirestore.DocumentSnapshot}
+ */
+_.get = async (id) => {
+  return await database.collection(collectionName).doc(id).get()
 }
 
-model.get = async (id, password) => {
-  const doc = await database.collection(collectionName).doc(id).get()
-  return formatResponse(doc.id, doc.data(), true, password)
-}
-
-model.getPage = async (id = null) => {
-  let docs = {}
+/**
+ * @param  {string} id
+ * @return {FirebaseFirestore.QuerySnapshot}
+ */
+_.getPage = async (id = null) => {
   if (id) {
     const prevLastDocs = await database.collection(collectionName).doc(id).get()
-    docs = await database.collection(collectionName)
+    return await database.collection(collectionName).where('deletedAt', '==', null)
                    .orderBy('createdAt', 'desc').startAfter(prevLastDocs).limit(20).get()
   } else {
-    docs = await database.collection(collectionName)
-                   .orderBy('createdAt', 'desc').limit(20).get()
+    return await database.collection(collectionName).where('deletedAt', '==', null).orderBy('createdAt', 'desc').limit(20).get()
   }
-
-  let res = []
-  docs.forEach(doc => {
-    res.push(formatResponse(doc.id, doc.data()))
-  })
-
-  return res
 }
 
-model.create = async (data) => {
+/**
+ * @param  data
+ * @return {string}
+ */
+_.create = async (data) => {
   const doc = await database.collection(collectionName).add(data)
   return doc.id
 }
 
-model.update = async (id, data) => {
+_.update = async (id, data) => {
   await database.collection(collectionName).doc(id).update(data)
-  return true
 }
 
-model.delete = async (id) => {
-  await database.collection(collectionName).doc(id).delete()
-  return true
+_.delete = async (id) => {
+  await database.collection(collectionName).doc(id).update({ deletedAt: new Data() })
 }
 
-model.getDateNarrow = async (year, month) => {
-  const docs = await database.collection(collectionName)
+_.getDateNarrow = async (year, month) => {
+  return await database.collection(collectionName)
                        .where('createdAt', '>=', new Date(year, month-1))
                        .where('createdAt', '<', new Date(year, month))
                        .orderBy('createdAt', 'desc')
                        .get()
-
-  let res = []
-  docs.forEach(doc => {
-    res.push(formatResponse(doc.id, doc.data()))
-  })
-
-  return res
 }
 
-module.exports = model
+module.exports = _
