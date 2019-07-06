d38a44736ab754137f3d55a4329b60a8cc2773d9
sidearrow <sidearrow1219@gmail.com>
2019-07-06 11:39:55 +0900

    commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,10 @@ $menus = [
     [ '掲示板', url('/bbs') ],
     [ 'みずとり会', url('/mizutori') ],
 ];
+
+$urlGitHub = 'https://github.com/sidearrow/hit-u-bad';
+$urlTwitter = 'https://twitter.com/hit_u_bad';
+
 ?>
 <!doctype html>
 <html>
@@ -21,7 +25,7 @@ $menus = [
         <meta name="author" content="sidearrow" />
 
         <link rel="stylesheet" href="{{ asset('/css/vendor/bootstrap/bootstrap.min.css') }}" />
-        <link rel="stylesheet" href="{{ asset('/css/vendor/fontawesome/all.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('/css/vendor/fontawesome/css/all.min.css') }}" />
         <script src="{{ asset('/js/vendor/jquery/jquery-3.4.0.slim.min.js') }}"></script>
         <script src="{{ asset('/js/vendor/bootstrap/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('/js/vendor/fontawesome/all.min.js') }}"></script>
@@ -30,20 +34,29 @@ $menus = [
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-dark fixed-top" style="background-color:#ac2926">
+            <nav class="navbar navbar-expand navbar-dark fixed-top" style="background-color:#ac2926">
                 <div class="container" style="max-width:720px">
                     <a class="navbar-brand" href="{{ url('/') }}">一橋大学バドミントン部</a>
-                    <div>a</div>
-                    <div class="d-flex">
-                        <div class="dropdown">
-                            <a class="btn btn-transpalent dropdown-toggle text-white" data-toggle="dropdown" style="cursor:pointer">MENU</a>
+                    <ul class="navbar-nav flex-row" style="font-size:1.2rem">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="{{ $urlTwitter }}" target="__blank">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="{{ $urlGitHub }}" target="__blank">
+                                <i class="fab fa-github"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown ml-3">
+                            <a class="nav-item nav-link dropdown-toggle text-white" data-toggle="dropdown" href="#">MENU</a>
                             <div class="dropdown-menu dropdown-menu-right">
                                 @foreach ($menus as $menu)
                                     <a class="dropdown-item" href="{{ $menu[1] }}">{{ $menu[0] }}</a>
                                 @endforeach
                             </div>
-                        </div>
-                    </div>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
