5f0b607323cf90b1c9da73bec43b9e46733f1ace
sidearrow <sidearrow1219@gmail.com>
2019-07-15 22:00:04 +0900

    commit

--- app/Services/Actions/IndexService.php
+++ app/Services/Actions/IndexService.php
@@ -13,7 +13,11 @@ class IndexService
         $this->viewData = new \stdClass();
 
         $this->viewData->topImages = $this->getTopImagesUrl();
-        $this->viewData->introWording = $this->getIntroWording();
+        //$this->viewData->introWording = $this->getIntroWording();
+        $this->viewData->introWording =
+            "2018年7月現在、私達一橋大学バドミントン部は男子22名、女子5名の計25名で活動しています。\n" .
+            "関東学生バドミントン連盟男子は4部、女子は5部に所属しています。\n" .
+            "男女・経験者大学始め関係なく、リーグでの昇格を目指して日々練習に励んでいます。";
     }
 
     public function getViewData()
