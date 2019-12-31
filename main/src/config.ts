1d9eac0a92005a08ca849d67c163192e7bcbbf08
sidearrow <sidearrow1219@gmail.com>
2019-12-31 11:19:42 +0900

    commig

--- main/src/config.ts
+++ main/src/config.ts
@@ -1,3 +1,3 @@
 export default {
-  googleSiteVerification: 'ieOa4SC9KQrBdJ5iB8u6e-_7q9EOfS2jj7lk65cI68g',
+  googleSiteVerification: 'IgCCQtyvTcRzPpT3YTUw7-0Zw3ef-2lCa6zcj0Mmoig'
 }
--- main/src/templates/league.tsx
+++ main/src/templates/league.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
 import Layout from '../components/layout';
-import leagueResultData from '../data/league_results.json';
 import SEO from '../components/seo';
 
-const Component: React.FC = ({ pageContext }) => {
-  console.log(pageContext)
-  const badge = (flg: Number) => {
-    if (flg === 0) {
+type Props = {
+  pageContext: {
+    data: {
+      year: string;
+      season: string;
+      data: {
+        league_id: string,
+        title: string,
+        gender: string,
+        year: string,
+        season: string,
+        group: string,
+        rank: string,
+        up_flg: string,
+        down_flg: string,
+        remarks: string,
+      }[]
+    }[]
+  }
+};
+
+const Component: React.FC<Props> = ({ pageContext }) => {
+  const badge = (upFlg: boolean, downFlg: boolean) => {
+    if (!upFlg && !downFlg) {
       return '';
     }
     return (
-      <span className={`px-2 border d-inline-block ${flg === 1 && 'border-primary text-primary'} ${flg === -1 && 'border-secondary text-secondary'}`}>{flg === 1 && '昇格'}{flg === -1 && '降格'}</span>
+      <span className={`px-2 border d-inline-block ${upFlg && 'border-primary text-primary'} ${downFlg && 'border-secondary text-secondary'}`}>{upFlg && '昇格'}{downFlg && '降格'}</span>
     )
   };
+
+  const getResultString = (
+    group: string,
+    rank: string,
+    reamrks: string
+  ) => reamrks === '' ? `${group} 部 ${rank} 位` : reamrks;
 
   return (
     <Layout isShowPageTitle={true} pageTitle="リーグ戦" pageSubTitle="関東学生バドミントン連盟リーグ、リーグ戦の結果です">
@@ -24,14 +49,12 @@ const Component: React.FC = ({ pageContext }) => {
           <th>女子</th>
         </thead>
         <tbody>
-          {leagueResultData.map((v, i) => (
-            v.season.map((vs, i) => (
-              <tr key={i}>
-                <td className="text-nowrap">{`${v.year} ${vs.season === '1' ? '春' : '秋'}`}</td>
-                <td><span className="pr-2">{vs.m}</span>{badge(vs.mFlg)}</td>
-                <td><span className="pr-2">{vs.f}</span>{badge(vs.fFlg)}</td>
-              </tr>
-            ))
+          {pageContext.data.map((v, i) => (
+            <tr key={i}>
+              <td className="text-nowrap">{`${v.year} ${v.season === '1' ? '春' : '秋'}`}</td>
+              <td><span className="pr-2">{getResultString(v.data[0].group, v.data[0].rank, v.data[0].remarks)}</span>{badge(v.data[0].up_flg === '1', v.data[0].down_flg === '1')}</td>
+              <td><span className="pr-2">{getResultString(v.data[1].group, v.data[1].rank, v.data[1].remarks)}</span>{badge(v.data[1].up_flg === '1', v.data[1].down_flg === '1')}</td>
+            </tr>
           ))}
         </tbody>
       </table>
