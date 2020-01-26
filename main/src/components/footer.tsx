76c271bdc4e7d86d9451675ad3f7a601d07b0fef
sidearrow <sidearrow1219@gmail.com>
2020-01-26 19:30:48 +0900

    スタイル修正

--- main/src/components/footer.tsx
+++ main/src/components/footer.tsx
@@ -5,7 +5,7 @@ import { Link } from 'gatsby';
 
 const Footer = () => (
   <footer className="footer py-5">
-    <div className="container">
+    <div className="container-fluid main-container">
       <div className="text-right">
         <span>
           <a href="https://twitter.com/hit_u_bad" target="__blank">
@@ -22,10 +22,10 @@ const Footer = () => (
       </div>
       <div className="mt-4">
         {footerLinkList.map((v) => (
-          <Link to={v.path} className="mr-3">{v.text}</Link>
+          <Link to={v.path} className="d-inline-block mr-3">{v.text}</Link>
         ))}
       </div>
-      <div className="text-center text-secondary mt-4 pt-4 border-top">{`© ${new Date().getFullYear()} 一橋大学バドミントン部`}</div>
+      <div className="text-center mt-4 pt-4 border-top">{`© ${new Date().getFullYear()} 一橋大学バドミントン部`}</div>
     </div>
   </footer>
 );
