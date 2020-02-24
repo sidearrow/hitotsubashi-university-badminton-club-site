acd634a437543d8ba0439c0b85db5a42f873b3f1
sidearrow <sidearrow1219@gmail.com>
2020-02-24 10:09:31 +0900

    mod footer

--- main/src/components/footer.tsx
+++ main/src/components/footer.tsx
@@ -6,26 +6,44 @@ import { Link } from 'gatsby';
 const Footer = () => (
   <footer className="footer py-5">
     <div className="container">
-      <div className="text-right">
-        <span>
-          <a href="https://twitter.com/hit_u_bad" target="__blank">
-            <i className="fab fa-twitter"></i>
-            <span className="ml-2">Twitter</span>
-          </a>
-        </span>
-        <span className="ml-4">
-          <a href="https://github.com/sidearrow/hit-u-bad" target="__blank">
-            <i className="fab fa-github"></i>
-            <span className="ml-2">GitHub</span>
-          </a>
-        </span>
+      <div className="columns">
+        <div className="column is-half">
+          {footerLinkList.map((v) => (
+            <div>
+              <Link to={v.path} className="d-inline-block mr-3">{v.text}</Link>
+            </div>
+          ))}
+        </div>
+        <div className="column is-half">
+          <div className="text-right">
+            <div>
+              <span className="icon">
+                <i className="far fa-envelope"></i>
+              </span>
+              <span>hit.bad.team2017@gmail.com</span>
+            </div>
+            <hr className="spacer is-1" />
+            <div>
+              <a href="https://twitter.com/hit_u_bad" target="__blank">
+                <span className="icon">
+                  <i className="fab fa-twitter"></i>
+                </span>
+                <span className="ml-2">Twitter</span>
+              </a>
+            </div>
+            <div>
+              <a href="https://github.com/sidearrow/hit-u-bad" target="__blank">
+                <span className="icon">
+                  <i className="fab fa-github"></i>
+                </span>
+                <span className="ml-2">GitHub</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="mt-4">
-        {footerLinkList.map((v) => (
-          <Link to={v.path} className="d-inline-block mr-3">{v.text}</Link>
-        ))}
-      </div>
-      <div className="text-center mt-4 pt-4 border-top">{`© ${new Date().getFullYear()} 一橋大学バドミントン部`}</div>
+      <hr className="spacer is-1" />
+      <div className="has-text-centered">{`© ${new Date().getFullYear()} 一橋大学バドミントン部`}</div>
     </div>
   </footer>
 );
